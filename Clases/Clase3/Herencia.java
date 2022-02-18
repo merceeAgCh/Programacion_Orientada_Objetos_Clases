7ed850ffcbda5860d834b80fb18af353027624b9
Mercedes AC <breakh_070795@hotmail.com>
2022-02-17 22:26:42 -0600

    Clase 3 de POO con JAVA -Herencia

--- Clases/Clase3/Herencia.java
+++ Clases/Clase3/Herencia.java
@@ -1,3 +1,26 @@
 public class Herencia{
+// crear objeto del main.
+}
 
+//por cada archivo .java se permite una clase publica.
+class Abuelito{
+	protected String colorOjos;
+	protected double altura;
+}
+
+class Madre extends Abuelito{
+	void bailar(){
+		
+	}
+}
+
+class Hija extends Madre{
+	/*crear metodo describir, mostrara en pantalla
+	las propiedades que hereda del abuelo y cuales de la madre*/
+}
+
+class Padre{
+	void bailar(){
+
+	}
 }
