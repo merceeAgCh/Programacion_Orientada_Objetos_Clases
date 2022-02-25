2b5e1751f3fc6619833c51c161befda061edc0d8
Mercedes AC <breakh_070795@hotmail.com>
2022-02-24 19:20:14 -0600

    Ejercicio clase 3 -Herencia- Sin concluir

--- Clases/Clase3/Herencia.java
+++ Clases/Clase3/Herencia.java
@@ -1,5 +1,9 @@
 public class Herencia{
 // crear objeto del main.
+	public static void main(String... args) {
+		Herencia hijaHereda = new Herencia();
+		hijaHereda.describirHerencia();
+	}
 }
 
 //por cada archivo .java se permite una clase publica.
@@ -17,6 +21,12 @@ class Madre extends Abuelito{
 class Hija extends Madre{
 	/*crear metodo describir, mostrara en pantalla
 	las propiedades que hereda del abuelo y cuales de la madre*/
+	public String describirHerencia(String abuelo, String mama){
+		abuelo = "Nada, las variables son unicas del abuelo";
+		System.out.println("Lo que la hija hereda del abuelo es: " + abuelo);
+		mama = "Hereda el bailar";
+		System.out.println("Lo que la hija hereda del abuelo es: " + mama);
+	}
 }
 
 class Padre{
